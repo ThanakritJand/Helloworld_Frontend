edc2a3aa1e07ae9035be15a0a65cbd4216f6f52d
ThanakritJand <ThanakritJand@users.noreply.github.com>
2026-03-18 00:04:27 +0700

    set up project

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+    <div class="flex items-center justify-center min-h-screen bg-gray-100">
+        <h1 class="text-[100px] text-red-500 font-bold">
+            Hello Nong Noonggg G.000
+        </h1>
+    </div>
+</template>
